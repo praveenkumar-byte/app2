dc6e238888110a14c15b48b9adb5a1fc7a89e1b1
praveenkumar-byte <praveenkumar6022@gmail.com>
2022-06-23 21:45:26 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+    <p>"I am learning React. My life is getting better. "</p>
+    </div>
+  );
+}
+
+
