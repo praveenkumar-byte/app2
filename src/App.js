5338e449a42177b6a7bcf9273c30a0e33ef11329
PRAVEEN KUMAR NALLAMUDI <61980270+praveenkumar-byte@users.noreply.github.com>
2022-06-23 22:05:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-
+import Para from "./components/Para";
 export default function App() {
   return (
-    <div className="App">
-    <p>"I am learning React. My life is getting better. "</p>
-    </div>
+    <>
+    <Para/>
+    </>
+    
   );
 }
-
 
